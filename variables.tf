accbf7b013a7d5d1cb8337d213aade58c3b5f5bf
Nick Jones <nick@dischord.org>
2019-04-08 12:35:02 +0100

    Include variable listing internal services

--- variables.tf
+++ variables.tf
@@ -1,5 +1,15 @@
-variable "ssh_key_pair" {}
+variable "ssh_key_pair" {
+  type = "string"
+  default = "deadline"
+}
+
 variable "number_of_masters" {
   type = "string"
   default = "1"
 }
+
+variable "internal_services" {
+  type = "list"
+  default = [ "80", "443","2181", "5050", "8080", "8181" ]
+}
+
