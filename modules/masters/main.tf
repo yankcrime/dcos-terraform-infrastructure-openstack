84839f480f1a9590342fb07b4bffbdea5166885e
Nick Jones <nick@dischord.org>
2019-04-11 12:24:32 +0100

    Add masters module

---
+++ modules/masters/main.tf
@@ -0,0 +1,14 @@
+module "dcos-master-instances" {
+  source = "../../modules/instance"
+  
+  cluster_name    = "${var.cluster_name}"
+  hostname_format = "${var.hostname_format}"
+  num             = "${var.num_masters}"
+  user_data       = "${var.user_data}"
+  image_name      = "${var.image_name}"
+  flavor_name     = "${var.flavor_name}"
+  network_id      = "${var.network_id}"
+  associate_public_ip_address = "${var.associate_public_ip_address}"
+  floating_ip_pool = "${var.floating_ip_pool}"
+
+}
