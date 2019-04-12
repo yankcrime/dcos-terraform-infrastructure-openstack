f992ed60fb0a537bd0131e0195a52492e23c1d15
Nick Jones <nick@dischord.org>
2019-04-12 13:43:52 +0100

    Include public agents loadbalancer configuration for testing

--- 30_machines.tf
+++ 30_machines.tf
@@ -14,13 +14,15 @@ module "dcos-security-groups" {
 }
 
 module "dcos-network" {
-  cluster_name = "testing"
   source = "./modules/network"
+
+  cluster_name = "testing"
   external_network_id = "c72d2f60-9497-48b6-ab4d-005995aa4b21"
 }
 
 module "dcos-bootstrap-instance" {
   source = "./modules/bootstrap"
+
   network_id = "${module.dcos-network.network_id}"
   cluster_name = "testing"
   associate_public_ip_address = true
@@ -29,6 +31,7 @@ module "dcos-bootstrap-instance" {
 
 module "dcos-master-instances" {
   source = "./modules/masters"
+
   network_id = "${module.dcos-network.network_id}"
   cluster_name = "testing"
   num_masters = "1"
@@ -36,6 +39,7 @@ module "dcos-master-instances" {
 
 module "dcos-public-agent-instances" {
   source = "./modules/public-agents"
+
   network_id = "${module.dcos-network.network_id}"
   cluster_name = "testing"
   associate_public_ip_address = true
@@ -45,16 +49,27 @@ module "dcos-public-agent-instances" {
 
 module "dcos-private-agent-instances" {
   source = "./modules/private-agents"
+
   network_id = "${module.dcos-network.network_id}"
   cluster_name = "testing"
   num_private_agents = "5"
 }
 
 module "dcos-lb-masters" {
+  source = "./modules/lb-masters"
+
   dcos_masters_ip_addresses = "${module.dcos-master-instances.private_ips}"
   network_id = "${module.dcos-network.network_id}"
   subnet_id = "${module.dcos-network.subnet_id}"
   security_group_id = ["${module.dcos-security-groups.security_group_id}"]
-  source = "./modules/lb-masters"
+}
+
+module "dcos-lb-public-agents" {
+  source = "./modules/lb-public-agents"
+
+  dcos_public_agents_ip_addresses = "${module.dcos-public-agent-instances.private_ips}"
+  network_id = "${module.dcos-network.network_id}"
+  subnet_id = "${module.dcos-network.subnet_id}"
+  security_group_id = ["${module.dcos-security-groups.security_group_id}"]
 }
   
